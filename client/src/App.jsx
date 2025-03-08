8600edc5b6cd2c1b2be12c65489b493b2ea746d8
Moksh <moksh13112006@gmail.com>
2025-03-08 23:59:38 +0530

    cart fucntionality + queryinh backend for all products

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,29 @@
 import "@radix-ui/themes/styles.css";
-import { Theme,Flex } from "@radix-ui/themes";
-import MyApp from "./pages/navbar"
-import Filters from "./pages/filters";
-import Products from "./pages/products";
+import { Theme, Flex } from "@radix-ui/themes";
+import NavBar from "./components/navbar.jsx";
+import Home from "./pages/home.jsx";  
+import Cart from "./pages/cart.jsx"
+import More from "./pages/more.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 function App() {
+  const [cart, setCart] = useState([]);
+  const [currentProduct, setcurrentProduct] = useState({});
     return (
-      <>
-        
-        <Theme accentColor="cyan" grayColor="sand" radius="large" scaling="100%" appearance="dark">
-        <Flex direction="column">
-	        <MyApp />
-          <Flex align="start">
-          <Filters/>
-          <Products/>
-          </Flex>
-        </Flex>
-        </Theme>
-      </>
+        <Router>
+            <Theme accentColor="cyan" grayColor="sand" radius="large" scaling="100%" appearance="dark">
+                <Flex direction="column">
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<Home cart={cart} setCart={setCart} setcurrentProduct={setcurrentProduct} />} />
+                        <Route path="/cart" element={<Cart cart={cart}/>} />
+                        <Route path="/more" element={<More currentProduct={currentProduct}/>} />
+                    </Routes>
+                </Flex>
+            </Theme>
+        </Router>
     );
 }
 
-export default App
+export default App;
